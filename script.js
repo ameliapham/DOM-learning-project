5705686c3a82e862853a03e33ac70b33c74a4601
ameliapham <ptt.huong.8895@gmail.com>
2023-12-21 23:48:54 +0100

    Create element

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const listItems = document.querySelectorAll('.list-items')
 for (i = 0; i < listItems.length; i++) {
     listItems[i].style.fontSize = '20px'
 }
+
+const ul = document.querySelector('ul')
+const li = document.createElement('li')
+
+ul.apprend(li)
