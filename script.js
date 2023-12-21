9de862d83fa72ba7cc28a48246e894e715921dfa
ameliapham <ptt.huong.8895@gmail.com>
2023-12-21 23:39:47 +0100

    Change font size with querySelectorAll

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const title = document.querySelector('#main-heading')
 title.style.color = '#000000' 
+
+const listItems = document.querySelectorAll('.list-items')
+for (i = 0; i < listItems.length; i++) {
+    listItems[i].style.fontSize = '20px'
+}
