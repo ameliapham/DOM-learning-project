7025c4b8e9f99b794214a8bda92993972d21e752
ameliapham <ptt.huong.8895@gmail.com>
2023-12-22 22:56:21 +0100

    Modify attributes of new adding element

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const title = document.querySelector('#main-heading')
 title.style.color = '#000000' 
 
+const ul = document.querySelector('ul')
+const li = document.createElement('li')
+ul.append(li)
+
+li.innerText = 'Sherlock Homes ss5'
+li.setAttribute('class', 'list-items')
+
 const listItems = document.querySelectorAll('.list-items')
 for (i = 0; i < listItems.length; i++) {
     listItems[i].style.fontSize = '20px'
 }
-
-const ul = document.querySelector('ul')
-const li = document.createElement('li')
-
-ul.append(li)
-
-li.innerText = 'Sherlock Homes ss5'
