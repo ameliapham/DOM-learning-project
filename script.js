314bd40838f9b91dea93d1c43ce1d61cfff0b311
ameliapham <ptt.huong.8895@gmail.com>
2023-12-23 23:35:43 +0100

    Add event Listeners to button 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Styling element --------------------
 const title = document.querySelector('#main-heading')
 title.style.color = '#000000' 
 
@@ -15,3 +16,8 @@ for (i = 0; i < listItems.length; i++) {
 }
 
 li.remove()
+
+// Event Listeners --------------------
+const eventBtn2 = document.querySelector("#btn2")
+const alertBtn = () => alert('Hello')
+eventBtn2.addEventListener("click", alertBtn)
