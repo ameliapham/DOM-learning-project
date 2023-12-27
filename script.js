83d74fe24e5b9657ee6053029af3feb2c1732e97
ameliapham <ptt.huong.8895@gmail.com>
2023-12-27 22:06:39 +0100

    Try to change the color when hover on the button but false

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const eventBtn2 = document.querySelector("#btn2")
 // const alertBtn = () => alert('Hello')
 eventBtn2.addEventListener("click", () => alert('Hello Minimazing World'))
 
-const newButtonColor = document.querySelector("#btn3")
+const newButtonColor = document.querySelectorAll(".button")
 const changeColor = () => {
     newButtonColor.style.backgroundColor = '#da8b4e',
     newButtonColor.style.color = 'white'
@@ -31,5 +31,8 @@ const revertColor = () => {
     newButtonColor.style.backgroundColor = ''
     newButtonColor.style.color = ''
 }
-newButtonColor.addEventListener("mouseover", changeColor)
-newButtonColor.addEventListener('mouseout', revertColor)
+for (i=0; i<newButtonColor.length; i++ ){
+    newButtonColor.addEventListener("mouseover", changeColor)
+    newButtonColor.addEventListener('mouseout', revertColor)
+}
+
